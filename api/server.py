7f02b64368841ea8741bd3b8147b205cdf8cd923
6ocra3 <6o3kmm@mail.ru>
2023-07-07 16:22:22 +0300

    add DBInteraction to Server

--- api/server.py
+++ api/server.py
@@ -2,13 +2,27 @@ import requests
 import threading
 import argparse
 
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from DB.interaction.interaction import DbInteraction
 from flask import Flask, request
 from utils import config_parser
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, db_host, db_port, db_user, db_password, db_name, rebuild_db=False):
         self.host = host
         self.port = port
+
+        self.db = DbInteraction(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        password=db_password,
+        db_name=db_name,
+        rebuild_db=rebuild_db
+    )
 
         self.app = Flask(__name__)
         self.app.add_url_rule("/shutdown", view_func=self.shutdown)
@@ -43,6 +57,12 @@ if __name__ == "__main__":
 
     server_host = config["SERVER_HOST"]
     server_port = config["SERVER_PORT"]
+    db_host = config["DB_HOST"]
+    db_port=config["DB_PORT"]
+    db_user=config["DB_USER"]
+    db_password=config["DB_PASSWORD"]
+    db_name=config["DB_NAME"]
+    rebuild_db=config["REBUILD_DB"]
     
-    server = Server(host=server_host, port=server_port)
+    server = Server(host=server_host, port=server_port, db_host=db_host, db_port=db_port, db_user=db_user, db_password=db_password, db_name=db_name, rebuild_db=rebuild_db)
     server.run_server()
