01cc1c152c4333ab3d93bdd06ab03db1423874b5
6ocra3 <6o3kmm@mail.ru>
2023-07-07 13:33:32 +0300

    delete wrong tables

--- DB/interaction/interaction.py
+++ DB/interaction/interaction.py
@@ -25,15 +25,9 @@ class DbInteraction:
         self.engine = self.mysql_connection.connection.engine
 
         if rebuild_db:
-            self.create_table_users()
-            self.create_table_musical_compositions()
-
-    def create_table_users(self):
-        if not self.engine.dialect.has_table(self.engine, "users"):
-            Base.metadata.tables["users"].create(self.engine)
-        else:
-            self.mysql_connection.execute_query("DROP TABLE IF EXISTS users")
-            Base.metadata.tables["users"].create(self.engine)
+            self.create_table_tasks()
+            self.create_table_list_order()
+            self.create_table_tracker_order()
 
     def create_table_tasks(self):
         if not self.engine.dialect.has_table(self.engine, "tasks"):
@@ -54,32 +48,6 @@ class DbInteraction:
         else:
             self.mysql_connection.execute_query("DROP TABLE IF EXISTS tracker_order")
             Base.metadata.tables["tracker_order"].create(self.engine)
-
-    def create_table_musical_compositions(self):
-        if not self.engine.dialect.has_table(self.engine, "musical_compositions"):
-            Base.metadata.tables["musical_compositions"].create(self.engine)
-        else:
-            self.mysql_connection.execute_query(
-                "DROP TABLE IF EXISTS musical_compositions"
-            )
-            Base.metadata.tables["musical_compositions"].create(self.engine)
-
-    def add_user_info(self, username, email, password):
-        user = User(
-            username=username,
-            password=password,
-            email=email,
-        )
-        self.mysql_connection.session.add(user)
-        return self.get_user_info(username)
-    
-    def get_user_info(self, username):
-        user =  self.mysql_connection.session.query(User).filter_by(username=username).first()
-        if user:
-            self.mysql_connection.session.expire_all()
-            return {"username": user.username, "email": user.email, "password": user.password}
-        else:
-            raise UserNotFoundException("User not found")
         
 
 
@@ -89,7 +57,6 @@ if __name__ == "__main__":
         port=3306,
         user="root",
         password="pass",
-        db_name="some_db",
+        db_name="planner_db",
         rebuild_db=True
     )
-    db.add_user_info("test", "test", "test")
